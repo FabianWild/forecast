42f7963b2ff7520db101b3b390fbae408b186834
FabianWild <Fabian.Wild1996@gmx.de>
2023-05-25 18:11:51 +0200

    wind und andere attribute

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let map = L.map("map", {
 
 // thematische Layer
 let themaLayer = {
-    forecast: L.featureGroup()
+    forecast: L.featureGroup(),
+    wind: L.featureGroup()
 }
 
 // Hintergrundlayer
@@ -22,7 +23,8 @@ let layerControl = L.control.layers({
     "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(map)
 }, {
-    "Wettervorhersage MET Norway": themaLayer.forecast.addTo(map)
+    "Wettervorhersage MET Norway": themaLayer.forecast.addTo(map),
+    "ECMWF Windlayer": themaLayer.wind.addTo(map)
 }).addTo(map);
 
 // Maßstab
@@ -35,12 +37,79 @@ async function showForecast(url, latlng) {
     let response = await fetch(url);
     let jsondata = await response.json();
     console.log(jsondata, latlng);
+
+    let current = jsondata.properties.timeseries[0].data.instant.details;
+    console.log(current);
+
+    let timestamp = new Date(jsondata.properties.meta.updated_at).toLocaleString();
+
+    let markup = `
+        <h4>"Aktuelles Wetter für ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}, ${timestamp}</h4>
+        <table>
+            <tr><td>Luftdruck (hPa): </td><td>${current.air_pressure_at_sea_level}</td></tr>
+            <tr><td>Temperatur in °C</td><td>${current.air_temperature}</td></tr>
+            <tr><td>Bewölkung</td><td>${current.cloud_area_fraction}</td></tr>
+            <tr><td>Luftfeuchtigkeit</td><td>${current.relative_humidity}</td></tr>
+            <tr><td>Windrichtung</td><td>${current.wind_from_direction}</td></tr>
+            <tr><td>Windgeschwindigkeit</td><td>${current.wind_speed}</td></tr>
+        </table>
+        
+        `;
+
+        for (let i=0; i <= 24; i+=3){
+             //console.log(timeseries[i]);
+             let icon = jsondata.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
+             let image = `icons/${icon}.svg`;
+             markup += `<img src = "${image}" style="width:32px;" title = "${jsondata.properties.timeseries[i].time.toLocaleString()}>`
+        }
+
+    
+
+        L.popup().setLatLng(latlng).setContent(markup).openOn(themaLayer.forecast);
 }
 
 
 // auf Kartenklick reagieren
 
 map.on("click", function(evt){
-    console.log(evt.latlng.lat);
-    console.log(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`)
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`
+    showForecast(url, evt.latlng);
 });
+
+map.fireEvent("click", {
+    latlng: L.latLng[ibk.lat, ibk.lng]
+})
+
+// Winddaten laden
+async function loadWind(url) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    console.log(jsondata)
+
+
+    L.velocityLayer({
+        displayValues: true,
+        displayOptions: {
+            // label prefix
+            velocityType: "",
+
+            // leaflet control position
+            position: "bottomright",
+
+            // no data at cursor
+            emptyString: "keine Daten vorhanden",
+
+            //  one of: ['ms', 'k/h', 'mph', 'kt']
+            speedUnit: "k/h",
+
+            // direction label prefix
+            directionString: "Windrichtung",
+
+            // speed label prefix
+            speedString: "Windgeschwindigkeit",
+        },
+        data: jsondata, // see demo/*.json, or wind-js-server for example data service
+    }).addTo(themaLayer.wind);
+}
+
+loadWind("https://geographie.uibk.ac.at/data/ecmwf/data/wind-10u-10v-europe.json");
